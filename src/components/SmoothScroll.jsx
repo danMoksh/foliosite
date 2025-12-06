f24e8a3fadf280e9ed6c339e825b4b6c452f1a08
danMoksh <astraperoo@gmail.com>
2025-12-06 11:28:09 +0530

    Update SmoothScroll.jsx

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -4,12 +4,12 @@ import Lenis from "lenis";
 const SmoothScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.5,
+      duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: "vertical",
       gestureDirection: "vertical",
       smooth: true,
-      mouseMultiplier: 1.5,
+      mouseMultiplier: 1,
       smoothTouch: false,
       touchMultiplier: 2,
     });
