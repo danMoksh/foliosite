968f60eaa9ea6d71379e9943a547219f379d1825
danMoksh <astraperoo@gmail.com>
2025-12-11 03:10:38 +0530

    resume added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,4 @@
 import { render } from 'preact';
 import App from './App.jsx';
-import Resume from './pages/Resume.jsx';
 import './index.css';
-
-// Simple routing
-const route = window.location.pathname;
-
-let component;
-if (route === '/resume') {
-  component = <Resume />;
-} else {
-  component = <App />;
-}
-
-render(component, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
